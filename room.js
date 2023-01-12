7fed8a68c8a92e8cfad9825f7b7b4933d9818f7f
heweixiang <touchfish.cc@outlook.com>
2023-01-12 10:18:01 +0800

    1

--- room.js
+++ room.js
@@ -17,6 +17,14 @@ const room = {
     const AvailableEnergy = ROOM.energyAvailable;
     // 获取房间storage剩余能量
     const StorageEnergy = ROOM.storage ? ROOM.storage.store[RESOURCE_ENERGY] : 0;
+    // 获取container总容量
+    const ContainerEnergy = ROOM.find(FIND_STRUCTURES, {
+      filter: (structure) => {
+        return structure.structureType === STRUCTURE_CONTAINER;
+      }
+    }).reduce((sum, container) => {
+      return sum + container.store[RESOURCE_ENERGY];
+    }, 0);
     // 获取当前房间升级进度
     const Progress = ROOM.controller.progress;
     // 获取当前房间升级进度总量
@@ -25,9 +33,9 @@ const room = {
     const ProgressPercent = (Progress / ProgressTotal * 100).toFixed(4);
     // ProgressTotal - Progress 转换成K和M
     const ProgressTotalMinusProgress = ProgressTotal - Progress;
-    let ProgressTotalMinusProgressK = ProgressTotalMinusProgress/1000000 > 1 ? `${ProgressTotalMinusProgress/1000000}M` : `${ProgressTotalMinusProgress/1000}K`;
+    let ProgressTotalMinusProgressK = ProgressTotalMinusProgress / 1000000 > 1 ? `${ProgressTotalMinusProgress / 1000000}M` : `${ProgressTotalMinusProgress / 1000}K`;
     console.log(`--------- ${ROOM} Level：${RCL} Progress：${ProgressTotalMinusProgressK} ${ProgressTotalMinusProgress} ${ProgressPercent}% ---------`);
-    console.log(`AvailableEnergy：${AvailableEnergy} StorageEnergy：${StorageEnergy}`);
+    console.log(`AvailableEnergy：${AvailableEnergy} StorageEnergy：${StorageEnergy} ContainerEnergy：${ContainerEnergy}`);
     // 获取container数量
     const containerNum = ROOM.find(FIND_STRUCTURES, {
       filter: (structure) => {
