167f9788779f2dc063e8291849afc5bdd9d34b03
何维想 <49872030+heweixiang@users.noreply.github.com>
2023-02-01 20:02:17 +0800

    1

--- roomBuildingWrok.js
+++ roomBuildingWrok.js
@@ -77,7 +77,7 @@ function TowerManagerLoop(ROOM, tower) {
         // 如果是墙壁
         if (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART) {
           // 如果血量低于
-          return structure.hits < 7000//Game.Config.RCL['LV' + ROOM.controller.level].Ramparts * 10000 * 0.25;
+          return structure.hits < 15000//Game.Config.RCL['LV' + ROOM.controller.level].Ramparts * 10000 * 0.25;
         }
         return structure.hits < structure.hitsMax * 0.7;
       }
